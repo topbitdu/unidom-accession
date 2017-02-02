d6af52fa5c7ea345650d369c899b663ff7e4794c
Topbit Du <topbit.du@gmail.com>
2017-02-02 11:18:31 +0800

    1, Improve the Post Fulfillment spec to remove the un-used source codes.

--- lib/rspec/models/unidom/accession/post_fulfillment_spec.rb
+++ lib/rspec/models/unidom/accession/post_fulfillment_spec.rb
@@ -6,29 +6,6 @@ describe Unidom::Accession::PostFulfillment, type: :model do
   after :each do
   end
 
-=begin
-  context '.validates' do
-
-    model_attributes = { name: 'Tim' }
-
-    it_behaves_like 'validates', model_attributes, :name,
-      {             } => 0,
-      { name: nil   } => 2,
-      { name: ''    } => 2,
-      { name: 'A'   } => 1,
-      { name: 'AA'  } => 0,
-      { name: 'AAA' } => 0,
-      { name: '0'   } => 1,
-      { name: '00'  } => 0,
-      { name: '000' } => 0,
-      { name: 0     } => 1,
-      { name: 'A'*(described_class.columns_hash['name'].limit-1) } => 0,
-      { name: 'A'*described_class.columns_hash['name'].limit     } => 0,
-      { name: 'A'*(described_class.columns_hash['name'].limit+1) } => 1
-
-  end
-=end
-
   context do
 
     model_attributes = {

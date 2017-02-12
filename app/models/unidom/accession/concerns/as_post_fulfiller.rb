df48a6d0236faf7de055e02dde403f208f937cbb
Topbit Du <topbit.du@gmail.com>
2017-02-12 09:39:19 +0800

    1, Improve the As Post Fulfiller concern for the document.

--- app/models/unidom/accession/concerns/as_post_fulfiller.rb
+++ app/models/unidom/accession/concerns/as_post_fulfiller.rb
@@ -10,6 +10,8 @@ module Unidom::Accession::Concerns::AsPostFulfiller
     has_many :post_fulfillments, class_name: 'Unidom::Accession::PostFulfillment', as:     :fulfiller
     has_many :fulfilled_posts,   through:    :post_fulfillments,                   source: :fulfilled, source_type: 'Unidom::Position::Post'
 
+    ##
+    # 让当前参与者在指定时间 at 开始履行岗位 post 。指定时间缺省为当前时间。
     def fulfill_post!(post, at: Time.now)
       raise ArgumentError.new('The post argument is required.') if post.blank?
       raise ArgumentError.new('The at argument is required.'  ) if at.blank?

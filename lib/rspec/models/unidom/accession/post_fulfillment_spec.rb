12cc29f91f9a476560feabe5cc791eead8626478
Topbit Du <topbit.du@gmail.com>
2017-02-20 10:07:24 +0800

    1, Improve the Post Fulfillment spec for the part_time scope, & the temporary scope.

--- lib/rspec/models/unidom/accession/post_fulfillment_spec.rb
+++ lib/rspec/models/unidom/accession/post_fulfillment_spec.rb
@@ -17,6 +17,18 @@ describe Unidom::Accession::PostFulfillment, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'scope', :part_time, [
+      { attributes_collection: [ model_attributes                        ], count_diff: 0, args: [ true  ] },
+      { attributes_collection: [ model_attributes                        ], count_diff: 1, args: [ false ] },
+      { attributes_collection: [ model_attributes.merge(part_time: true) ], count_diff: 1, args: [ true  ] },
+      { attributes_collection: [ model_attributes.merge(part_time: true) ], count_diff: 0, args: [ false ] } ]
+
+    it_behaves_like 'scope', :temporary, [
+      { attributes_collection: [ model_attributes                        ], count_diff: 0, args: [ true  ] },
+      { attributes_collection: [ model_attributes                        ], count_diff: 1, args: [ false ] },
+      { attributes_collection: [ model_attributes.merge(temporary: true) ], count_diff: 1, args: [ true  ] },
+      { attributes_collection: [ model_attributes.merge(temporary: true) ], count_diff: 0, args: [ false ] } ]
+
   end
 
 end

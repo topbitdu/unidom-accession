7e0531b9f925907c0f62b5f5056d8e42e9655d67
Topbit Du <topbit.du@gmail.com>
2017-03-18 15:55:38 +0800

    1, Improve the Post Fulfillment model to remove the un-used source codes.

--- app/models/unidom/accession/post_fulfillment.rb
+++ app/models/unidom/accession/post_fulfillment.rb
@@ -23,9 +23,7 @@ class Unidom::Accession::PostFulfillment < Unidom::Accession::ApplicationRecord
     assert_present! :fulfilled, fulfilled
     assert_present! :fulfiller, fulfiller
     assert_present! :opened_at, opened_at
-    #raise ArgumentError.new('Argument fulfilled is required.') if fulfilled.blank?
-    #raise ArgumentError.new('Argument fulfiller is required.') if fulfiller.blank?
-    #raise ArgumentError.new('Argument opened_at is required.') if opened_at.blank?
+
     self.create! fulfiller: fulfiller, fulfilled: fulfilled, opened_at: opened_at
   end
 

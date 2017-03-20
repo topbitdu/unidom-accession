5b8d195f08aeff84ccc473958d9161176e303c4b
Topbit Du <topbit.du@gmail.com>
2017-03-20 15:56:12 +0800

    1, Improve the As Post Fulfilled concern for the argument validation.

--- app/models/unidom/accession/concerns/as_post_fulfilled.rb
+++ app/models/unidom/accession/concerns/as_post_fulfilled.rb
@@ -3,7 +3,8 @@
 
 module Unidom::Accession::Concerns::AsPostFulfilled
 
-  extend ActiveSupport::Concern
+  extend  ActiveSupport::Concern
+  include Unidom::Common::Concerns::ArgumentValidation
 
   included do |includer|
 
@@ -16,8 +17,10 @@ module Unidom::Accession::Concerns::AsPostFulfilled
     # 或者
     # post.is_fulfilled_as_post! by: tom, at: Time.now
     def is_fulfilled_as_post!(by: nil, at: Time.now)
-      raise ArgumentError.new('The by argument is required.') if by.blank?
-      raise ArgumentError.new('The at argument is required.') if at.blank?
+      assert_present! :by, by
+      assert_present! :at, at
+      #raise ArgumentError.new('The by argument is required.') if by.blank?
+      #raise ArgumentError.new('The at argument is required.') if at.blank?
       post_fulfillments.create! fulfiller: by, opened_at: at
     end
 
@@ -27,8 +30,10 @@ module Unidom::Accession::Concerns::AsPostFulfilled
     # 或者
     # post.is_fulfilled_as_post? by: tom, at: Time.now
     def is_fulfilled_as_post?(by: nil, at: Time.now)
-      raise ArgumentError.new('The by argument is required.') if by.blank?
-      raise ArgumentError.new('The at argument is required.') if at.blank?
+      assert_present! :by, by
+      assert_present! :at, at
+      #raise ArgumentError.new('The by argument is required.') if by.blank?
+      #raise ArgumentError.new('The at argument is required.') if at.blank?
       post_fulfillments.fulfilled_by(by).valid_at(now: at).exists?
     end
 

46bf76788d783b1a631352577888ef0eca39e87b
Topbit Du <topbit.du@gmail.com>
2017-03-21 16:09:36 +0800

    1, Improve the As Post Fulfilled shared examples for the #is_fulfilled_as_post! method, & the #is_fulfilled_as_post? method.

--- lib/rspec/models/unidom/accession/concerns/as_post_fulfilled_shared_examples.rb
+++ lib/rspec/models/unidom/accession/concerns/as_post_fulfilled_shared_examples.rb
@@ -1,15 +1,80 @@
-shared_examples 'Unidom::Accession::Concerns::AsPostFulfilled' do |model_attributes|
+shared_examples 'Unidom::Accession::Concerns::AsPostFulfilled' do |model_attributes, post_fulfiller|
 
-  post_fulfillment_1_attribtues = {
-      fulfiller_id:   SecureRandom.uuid,
-      fulfiller_type: 'Unidom::Accession::Fulfiller::Mock'
-    }
+  before :each do
 
-  post_fulfillment_2_attribtues = {
-      fulfiller_id:   SecureRandom.uuid,
-      fulfiller_type: 'Unidom::Accession::Fulfiller::Mock'
-    }
+    @post = described_class.create! model_attributes
 
-  it_behaves_like 'has_many', model_attributes, :post_fulfillments, Unidom::Accession::PostFulfillment, [ post_fulfillment_1_attribtues, post_fulfillment_2_attribtues ]
+  end
+
+  after :all do
+
+    #@post.destroy
+
+  end
+
+=begin
+  context do
+
+    post = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', post, :is_fulfilled_as_post!, { by: post_fulfiller, at: Time.now }, [ :by, :at ]
+
+  end
+=end
+
+  context '#is_fulfilled_as_post!' do
+
+    it 'should reject the by argument = nil' do expect { @post.is_fulfilled_as_post! by: nil,            at: Time.now }.to raise_error(ArgumentError, 'The by argument is required.') end
+    it 'should reject the at argument = nil' do expect { @post.is_fulfilled_as_post! by: post_fulfiller, at: nil      }.to raise_error(ArgumentError, 'The at argument is required.') end
+
+    it "should be able to be fulfilled as post by #{post_fulfiller.inspect}" do
+      post_fulfillment = @post.is_fulfilled_as_post! by: post_fulfiller
+      expect(post_fulfillment).to     be_present
+      expect(post_fulfillment).to     be_a(Unidom::Accession::PostFulfillment)
+      expect(post_fulfillment).not_to be_new_record
+    end
+
+    it "should be able to be fulfilled as post by #{post_fulfiller.inspect}, at #{Time.now.inspect}" do
+      post_fulfillment = @post.is_fulfilled_as_post! by: post_fulfiller, at: Time.now
+      expect(post_fulfillment).to     be_present
+      expect(post_fulfillment).to     be_a(Unidom::Accession::PostFulfillment)
+      expect(post_fulfillment).not_to be_new_record
+    end
+
+  end
+
+  context '#is_fulfilled_as_post?' do
+
+    it 'should reject the by argument = nil' do expect { @post.is_fulfilled_as_post? by: nil,            at: Time.now }.to raise_error(ArgumentError, 'The by argument is required.') end
+    it 'should reject the at argument = nil' do expect { @post.is_fulfilled_as_post? by: post_fulfiller, at: nil      }.to raise_error(ArgumentError, 'The at argument is required.') end
+
+    it "should be able to be fulfilled as post by #{post_fulfiller.inspect}" do
+      expect(@post.is_fulfilled_as_post? by: post_fulfiller).to be_falsey
+      @post.is_fulfilled_as_post!        by: post_fulfiller
+      expect(@post.is_fulfilled_as_post? by: post_fulfiller).to be_truthy
+    end
+
+    it "should be able to be fulfilled as post by #{post_fulfiller.inspect}, at #{Time.now.inspect}" do
+      expect(@post.is_fulfilled_as_post? by: post_fulfiller).to be_falsey
+      @post.is_fulfilled_as_post!        by: post_fulfiller, at: Time.now
+      expect(@post.is_fulfilled_as_post? by: post_fulfiller).to be_truthy
+    end
+
+  end
+
+  context do
+
+    post_fulfillment_1_attribtues = {
+        fulfiller_id:   SecureRandom.uuid,
+        fulfiller_type: 'Unidom::Accession::Fulfiller::Mock'
+      }
+
+    post_fulfillment_2_attribtues = {
+        fulfiller_id:   SecureRandom.uuid,
+        fulfiller_type: 'Unidom::Accession::Fulfiller::Mock'
+      }
+
+    it_behaves_like 'has_many', model_attributes, :post_fulfillments, Unidom::Accession::PostFulfillment, [ post_fulfillment_1_attribtues, post_fulfillment_2_attribtues ]
+
+  end
 
 end
